312ef03bb9a83e4542ac1a0ab0eb6ea32a932080
xmonds <93551082+xmonds@users.noreply.github.com>
2024-01-18 12:09:02 -0600

    day 2

--- ReactNativeApp/App.js
+++ ReactNativeApp/App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+  // have javascript here
+  const {height, width} = Dimensions.get('window');
+  console.log("height" + height + " width " + width);
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#aaa',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textContainer: {
+      backgroundColor: 'lightgrey',
+      width: width*0.85, // width = 85% of screen
+    },
+    text: {
+      fontSize: 40,
+      color: 'purple',
+    },
+  });
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.textContainer}>
+        {/* comment */}
+        <Text style={styles.text}>Good Morning</Text>
+        <Text style= {{color: 'red', fontSize:20}}>healo</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
